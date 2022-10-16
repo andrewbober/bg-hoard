63ef85b5bcdc54b41a8e192a89053358d6288ddb
Andrew Bober <andy.bober@gmail.com>
2022-10-16 11:54:32 -0700

    Lab# 6

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { MatCardModule } from '@angular/material/card';
 import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
@@ -12,7 +11,13 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot([
+      {
+        path: 'game/:id', // <---- HERE
+        loadChildren: () =>
+            import('@bg-hoard/store/feature-game-detail').then(/* ... */)
+        }
+    ], { initialNavigation: 'enabledBlocking' }),
     MatCardModule,
     StoreUiSharedModule
   ],
